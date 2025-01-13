f1f694409a9c163af4bebe3071f229e15ba02205
abdoul nagib <abdoulnagib@gmail.com>
2025-01-13 14:20:43 +0000

    added category controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,38 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :destroy]
+  def index
+    @categories = Category.all
+  end
+
+  def show; end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, status: :see_other
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+end
